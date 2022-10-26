b011a75df594c06b7dd936eea898d1e64531b488
adithadirizki <adit.hadirizki@gmail.com>
2022-10-26 08:44:49 +0700

    Add metode topup alfamart/indomaret

---
+++ lib/src/api/topup_retail.dart
@@ -0,0 +1,57 @@
+import 'dart:convert';
+
+import 'package:json_annotation/json_annotation.dart';
+
+part 'topup_retail.g.dart';
+
+@JsonSerializable()
+class TopupRetailHistoryResponse {
+  @JsonKey(name: 'id')
+  late final int id;
+
+  @JsonKey(name: 'agenid')
+  late final String agenid;
+
+  @JsonKey(name: 'nohp')
+  late final String nohp;
+
+  @JsonKey(name: 'customer_name')
+  late final String customer_name;
+
+  @JsonKey(name: 'nominal')
+  late final double nominal;
+
+  @JsonKey(name: 'additionalfee')
+  late final double? additionalfee;
+
+  @JsonKey(name: 'payment_reff')
+  late final String? payment_reff;
+
+  @JsonKey(name: 'kode_pembayaran')
+  late final String? kode_pembayaran;
+
+  @JsonKey(name: 'channel')
+  late final String channel;
+
+  @JsonKey(name: 'status')
+  late final int status;
+
+  @JsonKey(name: 'sn')
+  late final String? sn;
+
+  @JsonKey(name: 'tanggal_bayar')
+  late final DateTime? tanggal_bayar;
+
+  @JsonKey(name: 'created_at')
+  late final DateTime created_at;
+
+  TopupRetailHistoryResponse();
+
+  factory TopupRetailHistoryResponse.fromString(String body) =>
+      _$TopupRetailHistoryResponseFromJson(json.decode(body) as Map<String, dynamic>);
+
+  factory TopupRetailHistoryResponse.fromJson(Map<String, dynamic> json) =>
+      _$TopupRetailHistoryResponseFromJson(json);
+
+  Map<String, dynamic> toJson() => _$TopupRetailHistoryResponseToJson(this);
+}
--- lib/src/screens/coin_mili.dart
+++ lib/src/screens/coin_mili.dart
@@ -8,6 +8,7 @@ import 'package:miliv2/src/api/api.dart';
 import 'package:miliv2/src/data/user_balance.dart';
 import 'package:miliv2/src/database/database.dart';
 import 'package:miliv2/src/models/topup.dart';
+import 'package:miliv2/src/models/topup_retail.dart';
 import 'package:miliv2/src/theme/colors.dart';
 import 'package:miliv2/src/utils/dialog.dart';
 import 'package:miliv2/src/utils/formatter.dart';
@@ -34,8 +35,11 @@ class CoinMiliScreen extends StatefulWidget {
 
 class _CoinMiliScreenState extends State<CoinMiliScreen> {
   final formKey = GlobalKey<FormState>();
-  List<TopupHistory> items = [];
+  PageController pageController = PageController(initialPage: 0);
+  List<TopupHistory> itemsTopup = [];
+  List<TopupRetailHistory> itemsTopupRetail = [];
   final TextEditingController textAmountController = TextEditingController();
+  String selectedMetode = 'TIKET';
   bool isLoading = true;
 
   Timer? timer;
@@ -55,10 +59,10 @@ class _CoinMiliScreenState extends State<CoinMiliScreen> {
       await initDB();
       //
       if (widget.openDetail != null) {
-        var idx = items
+        var idx = itemsTopup
             .indexWhere((element) => element.serverId == widget.openDetail);
         if (idx >= 0) {
-          detail(items[idx]);
+          detail(itemsTopup[idx]);
         }
       }
       //
@@ -82,20 +86,38 @@ class _CoinMiliScreenState extends State<CoinMiliScreen> {
     });
 
     await AppDB.syncTopupHistory();
+    await AppDB.syncTopupRetailHistory();
 
-    Condition<TopupHistory> filterDate = TopupHistory_.transactionDate
+    // Tiket transfer bank
+    Condition<TopupHistory> filterDateTopup = TopupHistory_.transactionDate
         .greaterOrEqual(dateRange.start.millisecondsSinceEpoch)
         .and(TopupHistory_.transactionDate.lessOrEqual(dateRange.end
-            .add(const Duration(hours: 24))
-            .millisecondsSinceEpoch));
+        .add(const Duration(hours: 24))
+        .millisecondsSinceEpoch));
 
-    Condition<TopupHistory> filterUser =
-        TopupHistory_.userId.equals(userBalanceState.userId);
+    Condition<TopupHistory> filterUserTopup =
+    TopupHistory_.userId.equals(userBalanceState.userId);
 
-    final db = AppDB.topupHistoryDB;
-    QueryBuilder<TopupHistory> query = db.query(filterDate.and(filterUser))
+    final dbTopup = AppDB.topupHistoryDB;
+    QueryBuilder<TopupHistory> queryTopup = dbTopup.query(filterDateTopup.and(filterUserTopup))
       ..order(TopupHistory_.transactionDate, flags: 1);
-    items = query.build().find();
+    itemsTopup = queryTopup.build().find();
+
+    // Topup Alfamart/Indomaret
+    Condition<TopupRetailHistory> filterDateTopupRetail = TopupRetailHistory_.created_at
+        .greaterOrEqual(dateRange.start.millisecondsSinceEpoch)
+        .and(TopupRetailHistory_.created_at.lessOrEqual(dateRange.end
+        .add(const Duration(hours: 24))
+        .millisecondsSinceEpoch));
+
+    Condition<TopupRetailHistory> filterUserTopupRetail =
+    TopupRetailHistory_.agenid.equals(userBalanceState.userId);
+
+    final dbTopupRetail = AppDB.topupRetailHistoryDB;
+    QueryBuilder<TopupRetailHistory> queryTopupRetail = dbTopupRetail
+        .query(filterDateTopupRetail.and(filterUserTopupRetail))
+      ..order(TopupRetailHistory_.created_at, flags: 1);
+    itemsTopupRetail = queryTopupRetail.build().find();
 
     setState(() {
       isLoading = false;
@@ -132,7 +154,7 @@ class _CoinMiliScreenState extends State<CoinMiliScreen> {
     });
   }
 
-  Widget buildHistoryItem(TopupHistory history) {
+  Widget buildTopupHistoryItem(TopupHistory history) {
     // bool isDebit = data.debitAmount > 0;
     // String description = data.description;
     // if (data.productName != null &&
@@ -188,6 +210,63 @@ class _CoinMiliScreenState extends State<CoinMiliScreen> {
     );
   }
 
+  Widget buildTopupRetailHistoryItem(TopupRetailHistory history) {
+    // bool isDebit = data.debitAmount > 0;
+    // String description = data.description;
+    // if (data.productName != null &&
+    //     data.productDetail != null &&
+    //     data.productDetail!.isNotEmpty) {
+    //   description =
+    //       'Pembelian ${data.productName!.trim()} ke ${data.productDetail!.trim()}';
+    // } else if (data.productName != null) {
+    //   description = '${data.productName!.trim()}';
+    // }
+    return Container(
+      padding: const EdgeInsets.only(left: 15, right: 15, bottom: 0, top: 10),
+      // color: Colors.white,
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Text(
+                formatDate(history.created_at),
+                style: Theme.of(context)
+                    .textTheme
+                    .bodyMedium!
+                    .copyWith(fontWeight: FontWeight.bold),
+              ),
+              const Spacer(),
+              Text(
+                formatNumber(history.nominal),
+                style: Theme.of(context)
+                    .textTheme
+                    .bodyMedium!
+                    .copyWith(fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+          const SizedBox(height: 5),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Flexible(
+                child: Text(
+                  history.isSuccess
+                      ? history.sn!
+                      : history.isFailed ? 'Gagal'
+                        : history.isExpired ? 'Kadaluarsa' : 'Pending',
+                  // style: Theme.of(context).textTheme.caption,
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 5),
+          const Divider(),
+        ],
+      ),
+    );
+  }
+
   void openFilterDate() async {
     var range = await dateRangeDialog(context,
         initial: dateRange, firstDate: firstDate);
@@ -200,8 +279,22 @@ class _CoinMiliScreenState extends State<CoinMiliScreen> {
     }
   }
 
-  Widget buildItems(BuildContext context) {
-    if (isLoading && items.isEmpty) {
+  void onMetodeChange(String? metode) {
+    if (metode != null) {
+      setState(() {
+        selectedMetode = metode;
+      });
+
+      if (metode == 'TIKET') {
+        pageController.jumpToPage(0);
+      } else {
+        pageController.jumpToPage(1);
+      }
+    }
+  }
+
+  Widget buildTopupHistoryItems(BuildContext context) {
+    if (isLoading && itemsTopup.isEmpty) {
       return const Center(
         child: CircularProgressIndicator(
           strokeWidth: 2,
@@ -211,16 +304,40 @@ class _CoinMiliScreenState extends State<CoinMiliScreen> {
 
     return RefreshIndicator(
       onRefresh: onRefresh,
-      child: items.isEmpty
+      child: itemsTopup.isEmpty
           ? const Center(
               child: Text('Tidak ada data'),
             )
           : ListView.builder(
-              itemCount: items.length,
+              itemCount: itemsTopup.length,
               itemBuilder: (context, index) {
-                return buildHistoryItem(items[index]);
+                return buildTopupHistoryItem(itemsTopup[index]);
               },
             ),
+    );
+  }
+
+  Widget buildTopupRetailHistoryItems(BuildContext context) {
+    if (isLoading && itemsTopupRetail.isEmpty) {
+      return const Center(
+        child: CircularProgressIndicator(
+          strokeWidth: 2,
+        ),
+      );
+    }
+
+    return RefreshIndicator(
+      onRefresh: onRefresh,
+      child: itemsTopupRetail.isEmpty
+          ? const Center(
+        child: Text('Tidak ada data'),
+      )
+          : ListView.builder(
+        itemCount: itemsTopupRetail.length,
+        itemBuilder: (context, index) {
+          return buildTopupRetailHistoryItem(itemsTopupRetail[index]);
+        },
+      ),
     );
   }
 
@@ -237,13 +354,59 @@ class _CoinMiliScreenState extends State<CoinMiliScreen> {
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           Container(padding: const EdgeInsets.all(10), child: const CoinCard()),
-          Flexible(
-            flex: 1,
-            child: Card(
-              // color: Colors.white,
-              child: buildItems(context),
-            ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Row(
+                children: [
+                  Radio<String>(
+                    onChanged: onMetodeChange,
+                    groupValue: selectedMetode,
+                    value: 'TIKET',
+                    activeColor: Colors.blueAccent,
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      onMetodeChange('TIKET');
+                    },
+                    child: const Text("Tiket"),
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  Radio<String>(
+                    onChanged: onMetodeChange,
+                    groupValue: selectedMetode,
+                    value: 'TOPUP',
+                    activeColor: Colors.blueAccent,
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      onMetodeChange('TOPUP');
+                    },
+                    child: const Text('Alfamart/Indomaret'),
+                  ),
+                ],
+              ),
+            ],
           ),
+          Expanded(
+              child: PageView(
+                controller: pageController,
+                onPageChanged: (value) {
+                  if (value == 0) {
+                    onMetodeChange('TIKET');
+                  } else if (value == 1) {
+                    onMetodeChange('TOPUP');
+                  }
+                },
+                children: [
+                  buildTopupHistoryItems(context),
+                  buildTopupRetailHistoryItems(context),
+                ],
+              ),
+          )
         ],
       ),
     );
