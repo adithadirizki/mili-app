df48e1d5c56ae7a46b37dd1883d21c8302761812
Dede Hidayat <dedehidayat.id@gmail.com>
2022-12-24 02:48:38 +0700

    [MILI v3.1.0] - Topup Koin Alfamart & Indomaret, setting harga jual modal

--- lib/src/config/config.dart
+++ lib/src/config/config.dart
@@ -7,8 +7,8 @@ class AppConfig {
 
   static String get baseUrl {
     if (kReleaseMode) {
-      return 'https://thanos.sridata.net';
-      // return 'http://api.mili-dev.sridata.net';
+      return 'https://thanos.sridata.net'; // LIVE / BETA
+      // return 'http://api.mili-dev.sridata.net'; // Staging
     }
     // return 'http://api.mili-dev.sridata.net';
     return 'http://192.168.20.104:8100/0';
